8bdaf511b9869c969f91f0f528c0e0326ada0d51
paris <jsucheol77@naver.com>
2025-07-02 06:51:58 +0900

    feat: 대기화면 추가

--- client/src/pages/gamecast/main/MainPage.tsx
+++ client/src/pages/gamecast/main/MainPage.tsx
@@ -2,16 +2,12 @@ import { Navigation } from "../../../components/gamecast/common/Navigation";
 import { Footer } from "../../../components/gamecast/common/Footer";
 import { NavigationCard } from "../../../components/gamecast/main/NavigationCard";
 
-interface Props {
-  setPage: (page: "main" | "participate" | "create" | "room") => void;
-}
-
-export const MainPage = ({ setPage }: Props) => {
+export const MainPage = () => {
   return (
     <div className="h-full flex flex-col justify-between bg-[linear-gradient(180deg,rgba(0,0,0,1)_0%,rgba(0,6,72,1)_100%)]">
       <Navigation />
       <main className="flex-1 flex items-center justify-center">
-        <NavigationCard onNavigate={setPage} />
+        <NavigationCard />
       </main>
       <Footer />
     </div>
